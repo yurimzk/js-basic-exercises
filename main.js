1d2cf8e473ab434abc68069ddd9e00465e2e9466
Yuri Tagomori <goncalvessyuri@gmail.com>
2024-04-03 19:03:01 -0300

    verify if is a boolean

--- main.js
+++ main.js
@@ -27,11 +27,17 @@
 // }
 
 // 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+// const myVar = "true";
+
+// if (typeof myVar === 'boolean') {
+//   console.log("É um booleano")
+// } else {
+//   console.log("Não é um booleano")
+// };
+
 
 // 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
-
 // 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
-
 // 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
 
 // 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
