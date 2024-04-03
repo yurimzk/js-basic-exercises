ad77a5469b5641343325e197808b279e57462fa1
Yuri Tagomori <goncalvessyuri@gmail.com>
2024-04-03 18:39:39 -0300

    add index html and main js

---
+++ main.js
@@ -0,0 +1,14 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+//     💡 Para saber o tipo de dado você pode usar o operador `typeof`
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
