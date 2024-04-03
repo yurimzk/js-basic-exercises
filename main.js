5035890ddef808e1c180d38b1157a4a53687519c
Yuri Tagomori <goncalvessyuri@gmail.com>
2024-04-03 19:05:44 -0300

    subtract, multiply and divide variables

--- main.js
+++ main.js
@@ -35,10 +35,16 @@
 //   console.log("Não é um booleano")
 // };
 
-
 // 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
 // 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
 // 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// const var1 = 12;
+// const var2 = 4;
+
+// alert(var1 - var2);
+// alert(var1 * var2);
+// alert(var1 / var2);
+
 
 // 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
 
