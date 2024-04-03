f25f151479335397d5fa14e523f35dacfa7bee51
Yuri Tagomori <goncalvessyuri@gmail.com>
2024-04-03 19:08:21 -0300

    verify if is odd or even

--- main.js
+++ main.js
@@ -47,5 +47,8 @@
 
 
 // 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
-
 // 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+// const myVar = 7;
+
+// const oddOrEven = (myVar % 2 === 0) ? "É um número par" : "É um número ímpar";
+// alert(oddOrEven)
